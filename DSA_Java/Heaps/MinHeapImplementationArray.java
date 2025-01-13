fd0d1647bce3161a7520143cb92838592e261774
Syamantak <Syamantak1305@gmail.com>
2025-01-13 15:03:05 +0530

    Remove function added in MinHeapImplementationArray

--- DSA_Java/Heaps/MinHeapImplementationArray.java
+++ DSA_Java/Heaps/MinHeapImplementationArray.java
@@ -28,7 +28,7 @@ class MinHeap {
     }
 
     public int peek() {
-        if(size == 0){
+        if (size == 0) {
             System.out.println("Heap empty");
             return -1;
         }
@@ -39,10 +39,36 @@ class MinHeap {
         arr[size++] = num;
         upHeapify(size - 1);
     }
+
+    public void downHeapify(int i) {
+        int lc = 2 * i + 1;
+        int rc = 2 * i + 2;
+        int minIndex = i;
+        if (arr[minIndex] > arr[lc]) {
+            minIndex = lc;
+        }
+        if (arr[minIndex] > arr[rc]) {
+            minIndex = rc;
+        }
+        if (i == minIndex)
+            return;
+        swap(i, minIndex);
+        downHeapify(minIndex);
+    }
+
+    public int remove() throws Exception {
+        if (size == 0)
+            throw new Exception("Heap is empty");
+        int peek = arr[0];
+        swap(0, size - 1);
+        size--;
+        downHeapify(0);
+        return peek;
+    }
 }
 
 public class MinHeapImplementationArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         MinHeap pq = new MinHeap(10);
 
         pq.add(1);
