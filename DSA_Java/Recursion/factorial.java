8338df2ddac22727e4939a7b56032b4e84f81e6a
Syamantak <Syamantak1305@gmail.com>
2025-02-10 15:11:39 +0530

    Factorial program was modified

--- DSA_Java/Recursion/factorial.java
+++ DSA_Java/Recursion/factorial.java
@@ -1,15 +1,21 @@
 import java.util.*;
+
 public class factorial {
-    public static int fact(int n){
-        if(n==1||n==0){return 1;} // base case
-        int ans=n*fact(n-1);    // recursive call with changed parameter
+    public static int fact(int n) {
+        if (n == 1 || n == 0) {
+            System.out.println("The factorial of 1 is: "+1);
+            return 1;
+        } // base case
+        int ans = n * fact(n - 1);    // recursive call with changed parameter
+        System.out.println("The factorial of "+n+" is: "+ans);
         return ans;
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
-        int n=sc.nextInt();
-        System.out.println(fact(n));
+        int n = sc.nextInt();
+        System.out.println("\nThe factorial of " + n + " is: " + fact(n));
         sc.close();
     }
 }
