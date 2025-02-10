9ee7de8ea2c380567c4b62c5695d5e0d3e2acf33
Syamantak <Syamantak1305@gmail.com>
2025-02-10 16:47:19 +0530

    printIncreasingAfterCalls program was modified

--- DSA_Java/Recursion/printIncreasingAfterCalls.java
+++ DSA_Java/Recursion/printIncreasingAfterCalls.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class printIncreasingAfterCalls {
- public static void dec(int n){
+ public static void inc(int n){
         if(n==0){return;}
-        dec(n-1);
+        inc(n-1);
         System.out.println(n);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
         int n=sc.nextInt();
-        dec(n);
+        inc(n);
         sc.close();
     }
 }
