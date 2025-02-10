e887642ab965809149563b45320ff6086052ed50
Syamantak <Syamantak1305@gmail.com>
2025-02-10 14:38:52 +0530

    FunctionCalls program was modified.

--- DSA_Java/Recursion/FunctionCalls.java
+++ DSA_Java/Recursion/FunctionCalls.java
@@ -1,19 +1,33 @@
-class FunctionCalls{
+class FunctionCalls {
+    // Static counters to track function calls
+    static int mangoCount = 0;
+    static int bananaCount = 0;
+    static int appleCount = 0;
+
     public static void mango() {
-        System.out.println("Hi,I am in mango");
+        mangoCount++; // Increment mango counter
+        System.out.println("Hi, I am in mango for " + mangoCount + " times");
     }
+
     public static void banana() {
-        mango();
-        System.out.println("Hi,I am in banana");
+        bananaCount++; // Increment banana counter
+        System.out.println("Hi, I am in banana for " + bananaCount + " times");
+        mango(); // Call mango from banana
     }
+
     public static void apple() {
-        banana();
-        System.out.println("Hi,I am in apple");
-        mango();
+        appleCount++; // Increment apple counter
+        System.out.println("Hi, I am in apple for " + appleCount + " times");
+        banana(); // Call banana from apple
+        mango();  // Call mango from apple
     }
+
     public static void main(String[] args) {
-        System.out.println("Hi,I am in main");
-        apple();
-        System.out.println("Hi,I am in main");
+        System.out.println("Starting function calls...");
+        apple(); // Start the chain of calls
+        System.out.println("Function calls completed.");
+        System.out.println("Mango was called " + mangoCount + " times.");
+        System.out.println("Banana was called " + bananaCount + " times.");
+        System.out.println("Apple was called " + appleCount + " times.");
     }
 }
