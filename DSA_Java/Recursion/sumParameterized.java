d58d0df712ddf6cf918ca45575138f23b1ec2ddc
Syamantak <Syamantak1305@gmail.com>
2025-02-12 11:07:38 +0530

    sumParameterized was modified to better understanding

--- DSA_Java/Recursion/sumParameterized.java
+++ DSA_Java/Recursion/sumParameterized.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class sumParameterized {
     public static void sum(int n,int s){
         if(n==0){
-            System.out.println(s);
+            System.out.println("The answer is: "+s);
             return;
         }
+        System.out.println(n+" + "+s+": "+(s+n));
         sum(n-1,s+n);
     }
     public static void main(String[] args) {
