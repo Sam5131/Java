fb6dac9347e53c486f36f36e984534d258647801
Syamantak <Syamantak1305@gmail.com>
2025-02-12 11:48:41 +0530

    subsets program was modified for clarity

--- DSA_Java/Recursion/subsets.java
+++ DSA_Java/Recursion/subsets.java
@@ -1,7 +1,7 @@
 public class subsets {
     public static void printSubsets(int i,String s,String ans) {
         if (i==s.length()) {
-            System.out.println(ans);
+            System.out.print(ans+" ");
             return;
         }
         char ch=s.charAt(i);
@@ -9,7 +9,7 @@ public class subsets {
         printSubsets(i+1, s, ans);  //doesnt include ch
     }
     public static void main(String[] args) {
-        String s="abcd";
+        String s="abca";
         printSubsets(0,s,"");
     }
 }
