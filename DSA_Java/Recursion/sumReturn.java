d15a09044d107bb05773e76710f3058874f9c03d
Syamantak <Syamantak1305@gmail.com>
2025-02-12 13:23:36 +0530

    sumReturn program was modifies for clarity

--- DSA_Java/Recursion/sumReturn.java
+++ DSA_Java/Recursion/sumReturn.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 public class sumReturn {
-    public static int sum(int n){
-        if(n==1||n==0){return n;} // base case
-        int ans=n+sum(n-1);    // recursive call with changed parameter
+    public static int sum(int n) {
+        if (n == 1 || n == 0) {
+            System.out.println(n);
+            return n;
+        } // base case
+        int prevSum = sum(n - 1);
+        int ans = n + prevSum;
+        System.out.println(n + " + " + prevSum + " = " + ans);
         return ans;
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
-        int n=sc.nextInt();
-        System.out.println(sum(n));
+        int n = sc.nextInt();
+        System.out.println("The answer is: " + sum(n));
         sc.close();
     }
 }
