5c5978f432aa58373f18d7759ef0ecca7b86b097
Syamantak <Syamantak1305@gmail.com>
2025-04-06 16:19:47 +0530

    BalancedBrackets program was updated.

--- DSA_Java/Stack/BalancedBrackets.java
+++ DSA_Java/Stack/BalancedBrackets.java
@@ -2,27 +2,40 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class BalancedBrackets {
-    public static boolean BalancedBracket(String s){
-        Stack<Character> st = new Stack<Character>();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch == '('){
-                st.push(ch);
-            }
-            else{
-                st.pop();
+
+    public static int countToBalance(String s) {
+        Stack<Character> stack = new Stack<>();
+        int unmatchedClosing = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    unmatchedClosing++;
+                }
             }
         }
-        if(st.isEmpty()){
-            return true;
-        }
-        return false;
+
+        // Remaining opening brackets in the stack are unmatched
+        int unmatchedOpening = stack.size();
+
+        return unmatchedOpening + unmatchedClosing;
     }
+
+    public static boolean isBalanced(String s) {
+        return countToBalance(s) == 0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string: ");
+        System.out.print("Enter a string: ");
         String s = sc.nextLine();
-        boolean ans = BalancedBracket(s);
-        System.out.println(ans);
+
+        boolean balanced = isBalanced(s);
+        System.out.println("Balanced: " + balanced);
+        System.out.println("To make it balanced, remove: " + countToBalance(s) + " bracket(s)");
     }
 }
