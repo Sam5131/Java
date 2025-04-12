9b1fcf6c2c809d1e76e57ca9ce7ded42196c9b67
Syamantak <Syamantak1305@gmail.com>
2025-04-12 11:30:01 +0530

    NextGreaterElement program was added.

---
+++ DSA_Java/Stack/NextGreaterElement.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static int[]nextGreaterElement(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        int[]res = new int[n];
+        res[n-1]=-1;
+        stack.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!stack.isEmpty() && arr[i]>stack.peek()){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                res[i] = -1;
+            }
+            else{
+                res[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[]arr = {1,2,3,3,1,6,2,8,1};
+        int[]result = nextGreaterElement(arr);
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        for(int ele : result){
+            System.out.print(ele+" ");
+        }
+    }
+}
