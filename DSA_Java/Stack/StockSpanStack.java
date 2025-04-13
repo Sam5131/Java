f8b03a1aaf5c8b9ba7123dde302c9e0b2c2273a8
Syamantak <Syamantak1305@gmail.com>
2025-04-13 14:23:40 +0530

    StockSpanStack program was edited.

--- DSA_Java/Stack/StockSpanStack.java
+++ DSA_Java/Stack/StockSpanStack.java
@@ -1,26 +1,29 @@
-import java.security.PublicKey;
 import java.util.Stack;
 
-class Pair{
-    int price;
-    int span;
+class StockSpanStack {
 
-    public Pair(int price,int span){
-        this.price = price;
-        this.span = span;
+    private static class Pair {
+        int price;
+        int span;
+
+        Pair(int price, int span) {
+            this.price = price;
+            this.span = span;
+        }
     }
-}
-class StockSpanStack{
+
     Stack<Pair> st;
-    public StockSpanStack(){
+
+    public StockSpanStack() {
         st = new Stack<>();
     }
-    public int next(int price){
+
+    public int next(int price) {
         int span = 1;
-        while(!st.isEmpty()&&st.peek().price<=price){
-            span = span + st.pop().span;
+        while (!st.isEmpty() && st.peek().price <= price) {
+            span += st.pop().span;
         }
-        st.push(new Pair(price,span));
+        st.push(new Pair(price, span));
         return span;
     }
 }
