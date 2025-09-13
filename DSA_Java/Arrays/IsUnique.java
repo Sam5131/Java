dfed7504faeb62cb7af07bfd68abe413baba2231
Syamantak <Syamantak1305@gmail.com>
2025-09-13 09:26:37 +0530

    isunique program was added.

---
+++ DSA_Java/Arrays/IsUnique.java
@@ -0,0 +1,21 @@
+public class IsUnique {
+    public static boolean check(String s){
+        //O(n) approach
+        if(s.length() > 128){
+            return false;
+        }
+        boolean[] char_set = new boolean[128];
+        for(int i = 0; i < s.length(); i++){
+            int val = s.charAt(i);
+            if(char_set[val]){
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "Syamntk";
+        System.out.println(check(s));
+    }
+}
